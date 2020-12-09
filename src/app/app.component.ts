506a6c92b9d3a446999bd8dde9ec91ad39a17c91
farshad bayat <farshad.bayat@gmail.com>
2020-12-09 17:09:34 +0330

    Add Pagination Size

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -39,6 +39,7 @@ export class AppComponent {
   pagination: TablePagination = {
     pageIndex: 0,
     pageSize: 10,
+    pageSizeOptions: [ 5, 10, 100, 1000, 10000]
   };
   printConfig: PrintConfig = {
     title: 'Print All Test Data',
