45667c803e8b36abebe49e3202f4eba0a77b6811
sinafrz <sina.firoozeh@ymail.com>
2023-05-13 19:01:14 +0330

    fix styles and progres

--- projects/dynamic-mat-table/src/lib/dynamic-mat-table/extensions/print-dialog/print-dialog.component.ts
+++ projects/dynamic-mat-table/src/lib/dynamic-mat-table/extensions/print-dialog/print-dialog.component.ts
@@ -3,7 +3,7 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { PrintConfig } from '../../../models/print-config.model';
 
 // tslint:disable-next-line:max-line-length
-const styles = 'body{margin:15px;}table{width:100%;border-collapse:collapse;}h2{text-align:center;}th.mat-header-cell{text-align:center;}div{text-align:center;margin:30px }tr{border-bottom:1px solid }td,th{padding:10px; text-align: center }.param-list{text-align: left;border:solid gray;border-width: 0px 0px 2px 0;margin-bottom: 10px;padding-bottom: 10px;}.param {display: inline-block;margin: 10px;}';
+const styles = 'body{margin:15px;}table{width:100%;border-collapse:collapse;}h2{text-align:center;}th.mat-mdc-header-cell{text-align:center;}div{text-align:center;margin:30px }tr{border-bottom:1px solid }td,th{padding:10px; text-align: center }.param-list{text-align: left;border:solid gray;border-width: 0px 0px 2px 0;margin-bottom: 10px;padding-bottom: 10px;}.param {display: inline-block;margin: 10px;}';
 
 @Component({
   // tslint:disable-next-line: component-selector
