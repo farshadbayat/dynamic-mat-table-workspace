934faef6e2f3c4ef7ca113a6bd4dfeb5c19a0286
sinafrz <sina.firoozeh@ymail.com>
2023-05-17 14:31:52 +0330

    use mat tooltip

--- projects/dynamic-mat-table/src/lib/modules/pagination/pagination.module.ts
+++ projects/dynamic-mat-table/src/lib/modules/pagination/pagination.module.ts
@@ -2,22 +2,18 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {PaginationComponent} from "./pagination.component";
 import {FormsModule} from "@angular/forms";
-import {TooltipDirective} from '../tooltip/tooltip.directive';
-import {TooltipComponent} from '../tooltip/tooltip.component';
-import {TemplateOrStringDirective} from '../tooltip/template-or-string.directive';
+import {MatTooltipModule} from '@angular/material/tooltip';
 
 
 
 @NgModule({
   declarations: [
     PaginationComponent,
-    TooltipDirective,
-    TooltipComponent,
-    TemplateOrStringDirective
   ],
   imports: [
     CommonModule,
     FormsModule,
+    MatTooltipModule,
   ],
     exports: [PaginationComponent]
 })
